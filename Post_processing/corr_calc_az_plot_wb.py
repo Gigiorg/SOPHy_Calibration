93dee4fb134a97ead353e0a94f04b09df1ee7f01
Gigiorg <gigiorg97@gmail.com>
2022-10-10 17:02:49 -0500

    Scatter plot added

--- Post_processing/corr_calc_az_plot_wb.py
+++ Post_processing/corr_calc_az_plot_wb.py
@@ -121,8 +121,90 @@ for root, direc, file in os.walk(PATH_TABLES):
                       ax_1.set_title(f"{file} - {i}")
                       
                       
-                      
-                      
+#%%
+
+#Search all the .xlsx files and iterate over each one to plot the comparative RCC}
+
+holder = np.array([])
+c_in_2 = np.array([])
+
+for root, direc, file in os.walk(PATH_TABLES):
+    for file in file:
+        if(file.endswith(".xlsx")):
+            df = pd.ExcelFile(PATH_TABLES+'\\'+file).parse('tabla')
+            
+            wr = np.array(df['RWF'])
+            wb = np.array(df['BWF'])
+            
+            
+            w = wr*wb
+            
+            c_in = df['C_initial']
+            c_in_db = np.array(df['C_initial [dB]'])
+            
+            
+            holder = np.append(holder,w) 
+            c_in_2 = np.append(c_in_2,c_in_db)
+            
+            '''
+            plt.figure(figsize=(10,10))
+            plt.scatter(w, c_in_db)
+            plt.grid()
+            plt.show()
+            '''
+            
+            
+            '''
+            f = len(pd.unique(df.Azimuth))
+            
+            if (f > 0):
                 
-                  
+                for i in sorted(pd.unique(df.Azimuth)):
+                      fig, ax = plt.subplots(figsize = (14,12))
+                        
+                      perf = df["Azimuth"] == i
+                      perf = df[perf]
+                      
+                      
+                      c_initial = perf['C_initial [dB]']
+                      c_after = perf["C_after Wr [dB]"]
+                      c_after_wb_only = perf["C_after Wb Only [dB]"]
+                      c_after_wb = perf["C_after Wb [dB]"]
+                      
+                      #c_after_wb = perf['Constant after Wb [dB]']
+                      std_initial = np.std(c_initial, ddof=1)
+                      std_after_wr = np.std(c_after, ddof=1)
+                      std_after_wb_only = np.std(c_after_wb_only, ddof=1)
+                      std_after_wb = np.std(c_after_wb, ddof= 1)
+                      
+                      
+                      date_time = pd.to_datetime(perf["Datetime"]) 
+                      
+                      #print(date_time)
+                      
+                      ax.scatter(date_time,c_initial)
+                      ax.plot(date_time,c_initial)
+                      
+                      ax.scatter(date_time,c_after)
+                      ax.plot(date_time,c_after)
+                      
+                      ax.scatter(date_time,c_after_wb_only)
+                      ax.plot(date_time,c_after_wb_only)
+                      
+                      ax.scatter(date_time,c_after_wb)
+                      ax.plot(date_time,c_after_wb)
+                      ax.grid()
+                      
+                      ax.set_title(f"{file}, {i}")
+                      ax.legend([f"Std: {std_initial}",f"Std: {std_after_wr}",
+                                 f"Std: {std_after_wb_only}",f"Std: {std_after_wb}"])   
+                
+                '''
           
+plt.figure(figsize=(10,10))
+plt.scatter(holder, c_in_2)
+plt.title("Scatter plot WrWb vs Pr*r^4")
+plt.xlabel("WrWb")
+plt.ylabel("Pr*r^4 [dB]")
+plt.grid()
+plt.show()
