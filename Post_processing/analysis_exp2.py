3db5da12648897ba45a7b7b160b26fa984a1d6aa
Gigiorg <gigiorg97@gmail.com>
2022-12-19 08:27:31 -0500

    RCC Script added

--- Post_processing/analysis_exp2.py
+++ Post_processing/analysis_exp2.py
@@ -33,7 +33,8 @@ if (f > 0):
         c_after_wb = perf["C_after Wb [dB]"]
         
         c_after_wb_lin = perf["C_after Wb"]
-            
+        
+        print(c_after_wb)
 
               
         std_initial = np.std(c_initial, ddof=1)
@@ -53,6 +54,7 @@ if (f > 0):
         
         #print(avg_c_after_wb)
                       
+        
         ax.scatter(date_time,c_after_wb, marker="x", linewidths=4)
         ax.plot(date_time,theo_constant, linestyle='dashed', markersize = 12, linewidth = 3, color = "red")
         ax.plot(date_time,avg_c_after_wb_db,linestyle='dashed', markersize = 12, linewidth = 3)
@@ -74,7 +76,104 @@ if (f > 0):
       
                       
 #%%
+#Filtering to only the Max Power Samples
 
+
+import os 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+#Post - processing
+
+PATH_5 = r"C:\Users\GIBS\Documents\Documents\SOPHy_Calibration\Processing\EXP2"
+
+
+df = pd.ExcelFile(PATH_5+'\\'+'Table_exp2.xlsx').parse('hits')
+        
+f = len(pd.unique(df.Azimuth))
+           
+
+if (f > 0):
+                
+    for i in sorted(pd.unique(df.Azimuth)):
+        fig, ax = plt.subplots(figsize = (11,9))
+                        
+        perf = df["Azimuth"] == i
+        perf = df[perf]
+        perf2 = perf["Count"] == 1
+        perf2 = perf[perf2]
+        exp_tick = df['Exp Constant [dB]'][0]          
+                      
+        c_initial = perf2['C_initial [dB]']
+        c_after = perf2["C_after Wr [dB]"]
+        c_after_wb = perf2["C_after Wb [dB]"]
+        
+        c_after_wb_lin = perf2["C_after Wb"]
+        
+        print(c_after_wb)
+
+              
+        std_initial = np.std(c_initial, ddof=1)
+        std_after_wr = np.std(c_after, ddof=1)
+        std_after_wb = round(np.std(c_after_wb, ddof= 1),2)
+                      
+                      
+        date_time = pd.to_datetime(perf2["Datetime"]) 
+        
+        theo_constant = perf2['Exp Constant [dB]']
+        theo_constant_lin = perf2["Exp Constant"]
+                      
+        #Mean 
+        avg_c_after_wb = np.mean(c_after_wb_lin)
+        avg_c_after_wb_db = 10*np.log10(avg_c_after_wb)*np.ones(len(theo_constant))
+        
+        
+        #print(avg_c_after_wb)
+                      
+        
+        ax.scatter(date_time,c_after_wb, marker="x", linewidths=4)
+        ax.plot(date_time,theo_constant, linestyle='dashed', markersize = 12, linewidth = 3, color = "red")
+        ax.plot(date_time,avg_c_after_wb_db,linestyle='dashed', markersize = 12, linewidth = 3)
+        #ax.scatter(date_time,c_after_wb_lin)
+        #ax.plot(date_time,theo_constant_lin)
+        
+        
+        
+        ax.grid()
+                      
+        ax.set_title(f"Experiment 2, Azimuth: {i}° - Only Max Power Echoes")
+        ax.legend([ "Theoretical Constant",f"Fitted Experimental Constant, Offset: {round(exp_tick-10*np.log10(avg_c_after_wb),2)} dB" ,
+                   f"Experimental Constant with Wr and Wb, Std: {std_after_wb}"])
+      
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Experimental RCC [dB]")
+        
+        ax.set_yticks([10*np.log10(avg_c_after_wb), exp_tick,60, 65, 70, 75, 80, 85])
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
 PATH_5 = r"C:\Users\GIBS\Documents\Documents\SOPHy_Calibration\Processing\EXP2"
 
 
---
+++ Post_processing/losses_char.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 15 10:13:31 2022
+
+@author: GIBS
+"""
+
+#%%
+
+import os 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+h_path = r'C:\Users\GIBS\Documents\Documents\SOPHy_Calibration\Processing\EXP2\Table_exp2.xlsx'
+v_path = r'C:\Users\GIBS\Documents\Documents\SOPHy_Calibration\Processing\EXP5\Table_exp5.xlsx'
+
+#Constants
+
+rEsf = 0.1765
+opFreq = 9.345e9 
+c = 3e8 
+
+
+pT1 = 200*(0.1/400)
+pT2 = 1.91
+
+gT = gR = 10**(38.5/10)
+lambdaRadar = round(c/opFreq, 3)      
+sigma = np.pi*rEsf**2
+gLNA1 = 10**(30/10)
+gLNA2 = 10**(40/10)
+
+
+
+#Horizontal
+
+df_h = pd.ExcelFile(h_path).parse('hits')
+
+
+rwf_h = np.array(df_h.RWF)
+bwf_h = np.array(df_h.BWF)
+rangeT_h = np.array(df_h.range)
+rPower_h = np.array(df_h["R Power [W]"])
+
+sqLosses_h = ((pT2*gT*gR*gLNA1*gLNA2*lambdaRadar**2)/((4*np.pi)**3*rangeT_h**4*rPower_h)) *sigma*rwf_h*bwf_h
+
+
+
+
+#Vertical
+
+
+df_v = pd.ExcelFile(v_path).parse('hits')
+
+
+rwf_v = np.array(df_v.RWF)
+bwf_v = np.array(df_v.BWF)
+rangeT_v = np.array(df_v.range)
+
+rPower_v = np.array(df_v["R Power [W]"])
+
+sqLosses_v = ((pT2*gT*gR*gLNA1*gLNA2*lambdaRadar**2)/((4*np.pi)**3*rangeT_v**4*rPower_v)) *sigma*rwf_v*bwf_v
